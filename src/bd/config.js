37d410c2f91bfbab0f8ab9d204aadf490be56f99
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2024-08-30 15:56:22 -0300

    Ajuste para o BD usar SSL

--- src/bd/config.js
+++ src/bd/config.js
@@ -2,11 +2,18 @@ const { Pool } = require('pg')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-let pool = new Pool({
-  connectionString: process.env.DATABASE_URL,  ssl: {
-    rejectUnauthorized: false,
-  }
-})
+let pool = null;
+if (isProduction) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL, ssl: {
+      rejectUnauthorized: false,
+    }
+  })
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
 
 
 module.exports = { pool }
